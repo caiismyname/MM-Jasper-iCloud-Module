022eb17f91e68bcf715c16afa83b58a28093fcbb
David Cai <davidcai2012@gmail.com>
2016-06-14 13:49:06 -0700

    Cleaning up extra words
    Import re is used in the regex search. It’s important

--- MM Jasper iCloud Module.py
+++ MM Jasper iCloud Module.py
@@ -1,3 +1,4 @@
+import re
 from pyicloud import PyiCloudService
 
 # For iCloud API
@@ -5,17 +6,14 @@ api = PyiCloudService('davidcai2012@gmail.com','cilantroLime7.03')
 iphone = api.devices['cetbS6ENhf8TSwHWRSpwmcI8L3J+C3USnmt6gCjV05FdE2jKVT8dNeHYVNSUzmWV']
 
 # For Jasper
-WORDS = ["IPHONE", "EYE", "PHONE", "PING", "FIND"]
+WORDS = ["IPHONE", "FIND"]
 
 def isValid(text):
 	# Seperate, all valid triggers
 	one = bool(re.search(r'\bIPHONE\b', text, re.IGNORECASE))
-	two = bool(re.search(r'\bEYE\b', text, re.IGNORECASE))
-	three = bool(re.search(r'\bPHONE\b', text, re.IGNORECASE))	
-	four = bool(re.search(r'\bPING\b', text, re.IGNORECASE))
-	five = bool(re.search(r'\bFIND\b', text, re.IGNORECASE))
+	two = bool(re.search(r'\bFIND\b', text, re.IGNORECASE))
 
-	if one or two or three or four or five:
+	if one or two:
 		return True
 	else:
 		return False
